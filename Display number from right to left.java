af07580cbcceac10d101fe725f41030f3e90ed67
praveena2602 <praveenap2605@gmail.com>
2025-08-04 08:40:13 -0700

    Display number from right to left.java

---
+++ Display number from right to left.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        while(n>0){
+            int c=n%10;
+            System.out.print(c+" ");
+            n=n/10;
+        }
+    }
+}
