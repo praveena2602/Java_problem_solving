7052ccc7397d69b3e4c2772af6a3496b0c94e461
praveena2602 <praveenap2605@gmail.com>
2025-08-09 00:36:00 -0700

    Accept the element into an array and replace every element in the array with the sum of its every other element.java

---
+++ accept the element into an array and replace every element in the array with the sum of its every other element.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+    public static void main(String[] args) {
+        int i,sum=0;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            sum=sum+a[i];
+         }
+        for(i=0;i<n;i++){
+            System.out.print(sum-a[i]+" ");
+        }
+}
+}
